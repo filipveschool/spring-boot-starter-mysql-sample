d765815702e996f22dc925cbe1a55ba90197d4e2
fvandeney <filip.vanden.eynde@devoteam.com>
2018-06-08 14:00:36 +0200

    Initial commit

---
+++ src/main/java/com/filip/springbootstartermysqlsample/SpringBootStarterMysqlSampleApplication.java
@@ -0,0 +1,12 @@
+package com.filip.springbootstartermysqlsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterMysqlSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarterMysqlSampleApplication.class, args);
+    }
+}
